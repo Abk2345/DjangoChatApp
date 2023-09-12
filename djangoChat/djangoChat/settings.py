23387f72ca0122211fb229cdf205e4f526a6a54a
Abk2345 <abhishantkumar138@gmail.com>
2023-09-13 00:11:32 +0530

    commit 2

--- djangoChat/djangoChat/settings.py
+++ djangoChat/djangoChat/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-sf9kg9@7s8$4ok4adm&@(zax(-feeo+bf=3j=a$w2k*2)7*d$%"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '0.0.0.0']
 
 
 # Application definition
